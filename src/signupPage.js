ba50d8b85578bb0de2542eb5622ed7a1fab53330
Srihari-Adiga <89541427+Srihari-Adiga@users.noreply.github.com>
2021-11-06 22:19:58 +0530

    Update signupPage.js

--- src/signupPage.js
+++ src/signupPage.js
@@ -1,7 +1,41 @@
 import "./signup.css";
 import loginAvatar from "./login_avatar.png";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import axios from "axios";
 
 function Signup() {
+  const history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    phone: "",
+    username: "",
+    password: "",
+    repassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const register = () => {
+    const { name, phone, username, password, repassword } = user;
+    if (name && phone && username && password && (password == repassword)) {
+      axios.post("http://localhost:8000/signuppage", user).then((res) => {
+        alert(res.data.message);
+        history.push("/");
+      });
+    } 
+    else {
+      alert("Invalid Input");
+    }
+  };
+
   return (
     <div className="loginPage">
       <div className="loginbox">
@@ -9,20 +43,51 @@ function Signup() {
         <h1>Sign Up</h1>
         <form action="login.html">
           <p>Name</p>
-          <input type="text" name="name" placeholder="Enter Name" />
+          <input
+            type="text"
+            name="name"
+            value={user.name}
+            placeholder="Enter Name"
+            onChange={handleChange}
+          />
           <p>Mobile Number</p>
-          <input type="text" name="phone" placeholder="Enter Mobile Number" />
+          <input
+            type="text"
+            name="phone"
+            value={user.phone}
+            placeholder="Enter Mobile Number"
+            onChange={handleChange}
+          />
           <p>Username</p>
-          <input type="text" name="username" placeholder="Enter Username" />
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            placeholder="Enter Username"
+            onChange={handleChange}
+          />
           <p>Password</p>
-          <input type="password" name="password" placeholder="Enter Password" />
-          <p>Confirm Password</p>
           <input
             type="password"
             name="password"
-            placeholder="Re-enter Password"
+            value={user.password}
+            placeholder="Enter Password"
+            onChange={handleChange}
           />
-          <input type="submit" name="signup" value="Sign-Up" />
+          <p>Confirm Password</p>
+          <input
+            type="password"
+            name="repassword"
+            value={user.repassword}
+            placeholder="Re-enter Password"
+            onChange={handleChange}
+          />
+          <input
+            type="button"
+            name="signup"
+            value="Sign-Up"
+            onClick={register}
+          />
           <br />
         </form>
       </div>
@@ -31,3 +96,4 @@ function Signup() {
 }
 
 export default Signup;
+
