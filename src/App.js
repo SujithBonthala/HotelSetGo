473616cc4cefffc419da14844ba1f76f44b6a316
Srihari-Adiga <89541427+Srihari-Adiga@users.noreply.github.com>
2021-11-06 22:17:34 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Aboutus from "./aboutUs";
 import Firstpage from "./firstPage";
+import Homehotel from "./homeHotel";
 import Loginpage from "./LoginPage";
 import Signup from "./signupPage";
+import { useState } from "react";
 
 function App() {
+  const [user, setLoginUser] = useState({});
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-        <Firstpage />
+          <Firstpage />
         </Route>
         <Route exact path="/aboutus">
           <Aboutus />
         </Route>
         <Route exact path="/loginpage">
-          <Loginpage />
+          <Loginpage setLoginUser={setLoginUser} />
         </Route>
         <Route exact path="/signuppage">
           <Signup />
+        </Route>
+        <Route exact path="/hotelhome">
+          {user && user._id ? (
+            <Homehotel setLoginUser={setLoginUser}/>
+          ) : (
+            <Loginpage setLoginUser={setLoginUser} />
+          )}
         </Route>
       </Switch>
     </Router>
@@ -28,3 +38,4 @@ function App() {
 }
 
 export default App;
+
