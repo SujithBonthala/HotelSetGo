455d7860bc233d567b92281d714c0c1fe4494b08
Srihari-Adiga <89541427+Srihari-Adiga@users.noreply.github.com>
2021-11-06 22:18:50 +0530

    updated login.js

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,9 +1,34 @@
 //import { useHistory } from "react-router-dom";
-import './LoginPage.css'
+import "./LoginPage.css";
 import loginAvatar from "./login_avatar.png";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-function Loginpage() {
-  //const history = useHistory();
+import axios from "axios";
+
+function Loginpage({ setLoginUser }) {
+  const history = useHistory();
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const login = () => {
+    axios.post("http://localhost:8000/loginpage", user).then((res) => {
+      alert(res.data.message);
+      setLoginUser(res.data.user);
+      history.push("/hotelhome");
+    });
+  };
+
   return (
     <div className="loginPage">
       <div className="loginbox">
@@ -11,10 +36,22 @@ function Loginpage() {
         <h1>Login</h1>
         <form>
           <p>Username</p>
-          <input type="text" name="username" placeholder="Enter Username" />
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            placeholder="Enter Username"
+            onChange={handleChange}
+          />
           <p>Password</p>
-          <input type="password" name="password" placeholder="Enter Password" />
-          <input type="submit" name="login" value="Login" />
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            placeholder="Enter Password"
+            onChange={handleChange}
+          />
+          <input type="button" name="login" value="Login" onClick={login} />
           <br />
           <a href="#">Forgot Password</a>
           <br />
